f012d6d8bc018ec24d5764cac9848e74036fc40e
NickVeles <xm1k123x@gmail.com>
2025-04-17 23:05:37 +0200

    feat: enhance InfoModal with animations and update CSS for visibility and background utilities

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactModal from "react-modal";
 import { Close } from "../components/Icons";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface InfoModalProps {
   isOpen: boolean;
@@ -9,25 +10,37 @@ interface InfoModalProps {
 
 const InfoModal: React.FC<InfoModalProps> = ({ isOpen, onRequestClose }) => {
   return (
-    <ReactModal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      contentLabel="Info Modal"
-      className="fixed inset-0 flex items-center justify-center bg-white"
-      shouldCloseOnEsc={true}
-    >
-      <div className="w-full h-full flex flex-col">
-        <button
-          className="text-gray-800 hover:text-gray-600 hover:cursor-pointer self-start"
-          onClick={onRequestClose}
+    <AnimatePresence>
+      {isOpen && (
+        <ReactModal
+          isOpen={isOpen}
+          onRequestClose={onRequestClose}
+          contentLabel="Info Modal"
+          className="fixed inset-0 flex items-center justify-center"
+          shouldCloseOnEsc={true}
+          ariaHideApp={false}
+          overlayClassName={"bg-transparent"}
         >
-          <Close className="w-12 h-12" />
-        </button>
-        <div className="flex justify-center items-center w-full h-full">
-          {/* Add modal content here */}
-        </div>
-      </div>
-    </ReactModal>
+          <motion.div
+            className="w-full h-full flex flex-col bg-white"
+            initial={{ y: '100%' }}
+            animate={{ y: 0 }}
+            exit={{ y: '100%' }}
+            transition={{ duration: 0.3 }}
+          >
+            <button
+              className="text-gray-800 hover:text-gray-600 hover:cursor-pointer self-start"
+              onClick={onRequestClose}
+            >
+              <Close className="w-12 h-12" />
+            </button>
+            <div className="flex justify-center items-center w-full h-full">
+              {/* Add modal content here */}
+            </div>
+          </motion.div>
+        </ReactModal>
+      )}
+    </AnimatePresence>
   );
 };
 
