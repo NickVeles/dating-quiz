69b7e4febff6c8a19b4d149eaae0a6b2a597627d
NickVeles <xm1k123x@gmail.com>
2025-04-19 00:31:13 +0200

    feat: add dummy onClick handler to open InfoModal from quiz message

--- src/App.tsx
+++ src/App.tsx
@@ -207,7 +207,7 @@ const App: React.FC = () => {
                         than welcome to share your results with me if you want
                         to.
                       </li>
-                      <li className="mb-2">
+                      <li className="mb-2" onClick={() => setIsInfoModalOpen(true)}> //TODO: delete onClick
                         Anyway, you might feel judged during this quiz
                         (unlucky). Take it with a grain of salt.
                       </li>
